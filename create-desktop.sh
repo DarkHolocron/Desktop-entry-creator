f401ec94eee1629d8186e15f908e32b0ee3b601c
DarkHolocron <darthbane327@gmail.com>
2026-07-16 21:00:13 +0300

    upd

--- create-desktop.sh
+++ create-desktop.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 read -p "Enter file name (without .desktop): " file_name
-while [[ -z "$file_name" ]] 
-do 
+while [[ -z "$file_name" ]]; do
     read -p "File name cannot be empty. Please try again: " file_name
 done
 read -p "Enter application name: " name
-while [[ -z "$name" ]] 
-do
+while [[ -z "$name" ]];do 
     read -p "Application name cannot be empty. Please try again: " name
 done
 read -p "Enter path to executable file: " exec
-while [[ -z "$exec" ]]
-do
+while [[ -z "$exec" ]]; do
     read -p "Executable path cannot be empty. Please try again: " exec
 done
 read -p "Enter path to icon file: " icon
-while [[ -z "$icon" ]]
-do
+while [[ -z "$icon" ]]; do
     read -p "Icon path cannot be empty. Please try again: " icon
 done
+while true; do
+    read -p "Show application in menu? (yes/no Enter = yes): " show_in_menu
+    if [[ -z "$show_in_menu" || "$show_in_menu" == "yes" ]]; then 
+        show_in_menu="false"
+        break
+    elif [[ "$show_in_menu" == "no" ]]; then   
+        show_in_menu="true"
+        break
+    else
+        echo "Error. Try again"
+    fi
+    done
 desktop_file="${file_name%.*}.desktop"
 echo "[Desktop Entry]" > "$desktop_file"
 echo "Version=1.1" >> "$desktop_file"
 echo "Type=Application" >> "$desktop_file"
 echo "Name=\"$name\"" >> "$desktop_file"
-echo "NoDisplay=false" >> "$desktop_file"
+echo "NoDisplay=$show_in_menu" >> "$desktop_file"
 echo "Icon=\"$icon\"" >> "$desktop_file"
 echo "Exec=\"$exec\"" >> "$desktop_file"
 echo "Terminal=false" >> "$desktop_file"
